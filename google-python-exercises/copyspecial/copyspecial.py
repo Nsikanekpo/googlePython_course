e61ab86f50f3086117bb18ecb98b433020c6968d
Nsikan <ekponsikan01@gmail.com>
2026-01-24 19:58:05 +0100

    Add copyspecial.py and logpuzzle.py exercises for file handling and puzzle image download

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -18,6 +18,41 @@ import subprocess
 # +++your code here+++
 # Write functions and modify main() to call them
 
+# LAB(begin solution)
+def get_special_paths(dirname):
+  """Given a dirname, returns a list of all its special files."""
+  result = []
+  paths = os.listdir(dirname)  # list of paths in that dir
+  for fname in paths:
+    match = re.search(r'__(\w+)__', fname)
+    if match:
+      result.append(os.path.abspath(os.path.join(dirname, fname)))
+  return result
+
+
+def copy_to(paths, to_dir):
+  """Copy all of the given files to the given dir, creating it if necessary."""
+  if not os.path.exists(to_dir):
+    os.mkdir(to_dir)
+  for path in paths:
+    fname = os.path.basename(path)
+    shutil.copy(path, os.path.join(to_dir, fname))
+    # could error out if already exists os.path.exists():
+
+
+def zip_to(paths, zipfile):
+  """Zip up all of the given files into a new zip file with the given name."""
+  cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
+  print("Command I'm going to do:" + cmd)
+  (status, output) = subprocess.getstatusoutput(cmd)
+  # If subprocess had a problem (status is non-zero),
+  # print its output to stderr and exit.
+  if status:
+    sys.stderr.write(output)
+    sys.exit(1)
+
+# LAB(end solution)
+
 
 
 def main():
@@ -44,12 +79,28 @@ def main():
     tozip = args[1]
     del args[0:2]
 
-  if not args: # A zero length array evaluates to "False".
+  if len(args) == 0:
     print('error: must specify one or more dirs')
-    sys.exit(1)
+    sys.exit(1)  
+
+  
 
   # +++your code here+++
   # Call your functions
+
+
+   # Gather all the special files
+  paths = []
+  for dirname in args:
+    paths.extend(get_special_paths(dirname))
+
+  if todir:
+    copy_to(paths, todir)
+  elif tozip:
+    zip_to(paths, tozip)
+  else:
+    print('\n'.join(paths))
+  # LAB(end solution)
 
 if __name__ == '__main__':
   main()
